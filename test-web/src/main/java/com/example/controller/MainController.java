486cf2183b986d5a1567a2ba4706719649517100
firechou <569287250@qq.com>
2018-05-15 21:16:54 +0800

    修改了hello方法

--- test-web/src/main/java/com/example/controller/MainController.java
+++ test-web/src/main/java/com/example/controller/MainController.java
@@ -1,9 +1,18 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+//@RestController
 public class MainController {
 	
-	public void hello() {
+	@RequestMapping(value="/hello", produces = "application/json; charset=utf-8")
+	@ResponseBody
+	public String hello() {
 		System.out.println("do something here ... ");
+		return "hello zhouyan ...啊啊";
 	}
 	
 }
